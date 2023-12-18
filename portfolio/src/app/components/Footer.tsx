f7d38133b7de9ce575033d279e801ed22355ce09
Fauzaan <fauzaan.mm@gmail.com>
2023-12-19 01:01:06 +0800

    apostrophe

--- portfolio/src/app/components/Footer.tsx
+++ portfolio/src/app/components/Footer.tsx
@@ -15,7 +15,7 @@ export function Footer() {
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Fauzaan's portfolio</span>
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">Fauzaan&apos;s portfolio</span>
             </div>
         </footer>
     )
--- portfolio/src/app/components/NavBar.tsx
+++ portfolio/src/app/components/NavBar.tsx
@@ -4,7 +4,7 @@ export function NavBar() {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="./" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="./favicon.ico" className="h-8" alt="Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fauzaan's Portfolio</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fauzaan&apos;s Portfolio</span>
                 </a>
                 {/* <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
