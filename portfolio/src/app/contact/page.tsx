432854438192b5a25dc3772eec18fa7726ad1f16
Fauzaan <fauzaan.mm@gmail.com>
2023-12-19 00:50:53 +0800

    Init

---
+++ portfolio/src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "../components/NavBar";
+
+export default function Page(){
+    return (
+        <div>
+            <NavBar />
+            This is my contact page
+        </div>
+    )
+}
---
+++ portfolio/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Fauzaan\'s portfolio',
+  description: 'A brief introduction to Fauzaan, and his interests',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className='bg-sky-950 min-h-screen'>{children}</body>
+    </html>
+  )
+}
