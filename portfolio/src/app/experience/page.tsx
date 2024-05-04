2b425f2a5731a2bc1e3f2676ed00f5fd1adcb8ca
Fauzaan <92146562+fauzxan@users.noreply.github.com>
2024-05-04 15:34:31 +0530

    fix: Change experience page title size

--- portfolio/src/app/experience/page.tsx
+++ portfolio/src/app/experience/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
 	return (
 		<div>
 			<NavBar />
-			<div className="mb-4 pt-20 font-extrabold tracking-wide leading-none text-black dark:text-white text-center text-3xll">
+			<div className="mb-4 pt-20 font-extrabold tracking-wide leading-none text-black dark:text-white text-center text-3xl">
 					Experience Page
 			</div>
 			<Footer />
