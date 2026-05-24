e030cdc714780ca68d5a9fa9548c90bad7008b8b
Fauzaan <fauzaan.mahaboob@ascenda.com>
2026-05-24 22:35:59 +0530

    remove: dead utility classes from hero

--- portfolio/src/app/components/Start.tsx
+++ portfolio/src/app/components/Start.tsx
@@ -3,13 +3,13 @@ import Particles from "../components/Particles";
 export default function Start() {
   return (
     <div className="flex flex-col p-12 items-center justify-center w-screen h-screen overflow-hidden">
-      <Particles className="absolute inset-0 animate-fade-in duration-10000" quantity={200} />
-      <h1 className="z-10 animate-fade-in text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+      <Particles className="absolute inset-0" quantity={200} />
+      <h1 className="z-10 text-4xl text-transparent bg-white cursor-default sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Hi there!
       </h1>
       {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /> */}
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-md text-zinc-750 ">Welcome to my website!</h2>
+      <div className="my-16 text-center">
+        <h2 className="text-md text-zinc-700 ">Welcome to my website!</h2>
       </div>
     </div>
   );
