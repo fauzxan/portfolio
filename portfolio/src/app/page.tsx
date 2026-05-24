f4c29510d6a344104c24df1d4a7006c72d9dd490
Fauzaan <fauzaan.mahaboob@ascenda.com>
2026-05-24 22:36:13 +0530

    remove: invisible CustomCursor component

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,14 +3,12 @@ import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 import Jumbotron from "./components/Jumbotron";
 import Carousel from "./components/ProjectCarousel";
-import CustomCursor from "./components/CustomCursor";
 import Particles from "./components/Particles";
 import Start from "./components/Start";
 
 export default function Home() {
   return (
     <main className="">
-      <CustomCursor />
       <NavBar />
       <Start />
 	  <Jumbotron />
